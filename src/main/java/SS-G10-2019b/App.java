762c33904d366bab719d0f4cd2340b8f472ddda4
pabloito <pradnic@itba.edu.ar>
2020-02-07 15:57:11 -0300

    Project Init

---
+++ src/main/java/SS-G10-2019b/App.java
@@ -0,0 +1,13 @@
+package SS-G10-2019b;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/SS-G10-2019b/AppTest.java
@@ -0,0 +1,20 @@
+package SS-G10-2019b;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
