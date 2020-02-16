f44c48e538679ae8d0364056cad676dfa0838e95
Ariel <ari.atar1997@gmail.com>
2020-02-16 12:33:46 -0300

    front2BackGroupsConfig

--- pythonScripts/configCreator.py
+++ pythonScripts/configCreator.py
@@ -67,6 +67,39 @@ def groupsConfig(f):
     for i in range(len(passengers)):
         f.write(passengers[i])
 
+# Configuracion por grupos Adelante para Atras
+def front2BackGroupsConfig(f):
+    groups = 4
+    group_lengths = []
+    extra_rows = lastrow%groups
+    base_group_length = lastrow//groups
+
+    while extra_rows > 0:
+        group_lengths.append(base_group_length+1)
+        extra_rows -= 1
+    while len(group_lengths)<groups:
+        group_lengths.append(base_group_length)
+
+    acum = 1
+    passengers = []
+    for j in range(groups):
+
+        group_j_passengers = []
+        group_j_rows = range(acum, acum + group_lengths[j])
+
+        for i in group_j_rows:
+            for s in seats:
+                group_j_passengers.append(str(i)+s+'\n')
+
+        random.shuffle(group_j_passengers)
+        for i in range(len(group_j_passengers)):
+            passengers.append(group_j_passengers[i])
+
+        acum += group_lengths[j]
+
+    for i in reversed(range(len(passengers))):
+        f.write(passengers[i])
+
 #FILE BUILDER
 configsFunctions = [randomConfig, back2FrontConfig, front2BackConfig, windowMiddleAisleConfig, groupsConfig]
 iterationIndex = 0
